d9b913a7f348710ac1cf5264daf0d3366b7f80af
mloyolatorres <62665610+mloyolatorres@users.noreply.github.com>
2021-01-31 18:57:50 -0500

    Add Controller para autos

---
+++ AutopartesPeru/Controllers/AutoparteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutopartesPeru.Controllers
+{
+    public class AutoparteController : Controller
+    {
+        // GET: Autoparte
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AutopartesPeru/Models/Auto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutopartesPeru.Models
+{
+    public class Auto
+    {
+        public int autoId { get; set; }
+
+        public string descripcion { get; set; }
+
+        public string año { get; set; }
+             
+    }
+}
